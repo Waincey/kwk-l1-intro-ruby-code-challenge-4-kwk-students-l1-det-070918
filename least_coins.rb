74eb8e8f19ed3b27c0be6e3309a6e2403158113c
Waincey <waincey.chan@gmail.com>
2018-07-12 15:43:58 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -2,6 +2,10 @@
 
 def least_coins(cents)
   least_coins = {:quarters=> 0.25, :dimes=> 0.10, :nickels=> 0.05, :pennies=>0.01} 
+if cents >= 25 
+  puts "add a quarter"
+  elsif cents 
+else 
 
 end
 least_coins(29)
